a5295f03a9754874afe1f43dce4eed37cd5e01f8
Sachin <Sachin333777@outlook.com>
2023-02-17 11:14:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector(".results");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () =>{
+        const playerSelection = button.classList[0];
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        results.innerHTML = result;
+    })
+
+});
+
+
+const choice = ['rock', 'paper', 'scissors'];
+function getComputerChoice(){
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+      } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+      ) {
+        return "You win!";
+      } else {
+        return "Computer wins!";
+      }
+
+      
+}
+
+
+
+
+
+
