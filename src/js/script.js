00949d30e35ed575c21e580d949416c1fcca57d4
Sachin <sachin333777@outlook.com>
2024-01-27 19:46:27 +0530

    set up the html for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -19,19 +19,15 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
-      } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-      ) {
-        return "You win!";
-      } else {
-        return "Computer wins!";
-      }
+        return "It's a tie!"; } else if ( (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) { return "You win!"; } else { return "Computer wins!"; }
 
-      
-}
+
+
+
+
+
+
+
 
 
 
