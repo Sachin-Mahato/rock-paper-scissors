312c102c190875feade0c8d2f778c85330eb7b45
Sachin <sachin333777@outlook.com>
2024-02-02 09:36:42 +0530

    m made mobile responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,41 @@
-const buttons = document.querySelectorAll("button");
-const results = document.querySelector(".results");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () =>{
-        const playerSelection = button.classList[0];
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        results.innerHTML = result;
-    })
-
-});
-
-
-const choice = ['rock', 'paper', 'scissors'];
-function getComputerChoice(){
-    return choice[Math.floor(Math.random() * choice.length)];
+const buttons = document.querySelectorAll('[data-id="btn"]');
+let score = document.querySelector('[data-id="score"]');
+const game = ['rock', 'paper', 'scissors'];
+const ruleButton = document.querySelector('[data-id="rule__btn"]');
+console.log(score.innerText)
+const choice = {
+  computerChoice: undefined,
+  userChoice: undefined,
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection) {
-        return "It's a tie!"; } else if ( (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) { return "You win!"; } else { return "Computer wins!"; }
 
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let {computerChoice, userChoice} = choice;
+        computerChoice = game[Math.floor(Math.random() * game.length)];
+        userChoice = button.getAttribute('id');
+        // console.log(computerChoice)
+    gameDecision(computerChoice, userChoice);
+    });
+});
+// console.log(choice.userChoice);
+
+function gameDecision(compChoice, usrChoice) {
+    let computerChoice = compChoice;
+    let userChoice = usrChoice;
+    if (compChoice === 'rock' && usrChoice === 'scissors' || compChoice === 'scissors' && userChoice === 'paper' || compChoice === 'paper' && usrChoice === 'rock' ) {
+        let scoreValue = parseFloat(score.innerText);
+        scoreValue += 1;
+        score.innerText = scoreValue;
+    };
+};
 
 
-
-
-
-
-
-
-
-
-
-
+ruleButton.addEventListener('click', () => {
+    const hide = ruleButton.parentElement.parentElement.querySelector('.game__img-container');
+    const open = hide.querySelector('.rules__instruction');
+    console.log(hide);
+    hide.style.display = 'none';
+    open.style.display = 'block';
+})
